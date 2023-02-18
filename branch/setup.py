448a4154673e3acfe84e791b1dcc8c1eb4df2dd4
Emma McCallan <emccallan47@qub.ac.uk>
2023-02-18 17:51:25 +0000

    ci file update

---
+++ branch/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Branch Naming',
+      version='1.0',
+      description='Python Validate Branch Naming Script',
+      author='Emma McCallan',
+      author_email='emccallan47@qub.ac.uk',
+      url='https://github.com/Emma121288/UniProject/tree/main/branch',
+     )
