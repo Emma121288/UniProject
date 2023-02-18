5d53ca6308a46b8363f4328151fa57c1e72581b6
Emma McCallan <emccallan47@qub.ac.uk>
2023-02-18 20:37:56 +0000

    files deleted

--- branch/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='Branch Naming',
-      version='1.0',
-      description='Python Validate Branch Naming Script',
-      author='Emma McCallan',
-      author_email='emccallan47@qub.ac.uk',
-      url='https://github.com/Emma121288/UniProject/tree/main/branch',
-     )
