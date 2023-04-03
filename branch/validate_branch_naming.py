766387f3db8a0af287fb1b47fed0cab4a84aa757
EmmaMcC <76816350+Emma121288@users.noreply.github.com>
2023-04-03 11:41:24 +0100

    Update validate_branch_naming.py

--- branch/validate_branch_naming.py
+++ branch/validate_branch_naming.py
@@ -7,6 +7,10 @@ config.read('config.ini')
 
 acronym_set = set(config.get('settings', 'acronym_set').split(','))
 category = set(config.get('settings', 'category').split(','))
+team_name_regex = re.compile(config.get('regex', 'team_name_regex'))
+work_category_regex = re.compile(config.get('regex', 'work_category_regex'))
+task_number_regex = re.compile(config.get('regex', 'task_number_regex'))
+task_description_regex = re.compile(config.get('regex', 'task_description_regex'))
 
 def validate_branch_name(branch_name):
     if branch_name == 'main':
@@ -25,31 +29,27 @@ def validate_branch_name(branch_name):
     task_number = task[0]
     task_description = task[1]
 
-    #validate team name
-    team_name_regex = re.compile(r"^[a-z]{3,12}$")
+    # validate team name
     team_name_match = team_name_regex.match(team_name)
     if not team_name_match:
         return False
 
-    #validate work category
-    work_category_regex = re.compile(r"^[a-z]{3,12}$")
+    # validate work category
     work_category_match = work_category_regex.match(work_category)
     if not work_category_match:
         return False
 
-    #validate task number
-    task_number_regex = re.compile(r"^[\d]{5,10}$")
+    # validate task number
     task_number_match = task_number_regex.match(task_number)
     if not task_number_match:
         return False
 
-    #validate task description
-    task_description_regex = re.compile(r"^[A-Z][a-zA-Z0-9]{3,24}$")
+    # validate task description
     task_description_match = task_description_regex.match(task_description)
     if not task_description_match:
         return False
 
-    #validate branch name
+    # validate branch name
     if work_category in category:
         pass
     else:
@@ -69,27 +69,3 @@ if __name__ == "__main__":
         print("CI_COMMIT_BRANCH environment variable is not set")
         exit(1)
 
- # exmaple valid branch names
-example_branch_name = "bacchus/feature/721023-D278ImplementnewFeature"
-example_branch_name2 = "bacchus/feature/55325-AbcdefGhijklMnopqrs"   
-
-# exmaple invalid branch names
-example_branch_name_breaking_convention = "bacchus12345/featuretobeimplemented/44324-abcdef"
-example_branch_name_breaking_convention = "bacchus12345/featuretobeimplemented/44324-abcdef-ghijklmnop"
-
-# validate script
-
-if validate_branch_name(example_branch_name):
-    print("Valid branch name")
-else:
-    print("Invalid branch name")
-
-if validate_branch_name(example_branch_name2):
-    print("Valid branch name")
-else:
-    print("Invalid branch name")
-
-if validate_branch_name(example_branch_name_breaking_convention):
-    print("Valid branch name")
-else:
-    print("Invalid branch name")
