d4639451b82a63ade351ed1943fcfbc315d078d7
EmmaMcC <76816350+Emma121288@users.noreply.github.com>
2023-04-03 16:21:25 +0100

    Update validate_pipeline_naming.py

--- pipeline/validate_pipeline_naming.py
+++ pipeline/validate_pipeline_naming.py
@@ -1,12 +1,17 @@
-import os
 import re
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read('config.ini')
+
+repo_name_regex = re.compile(config.get('pipeline_naming', 'repo_name_regex'))
+description_regex = re.compile(config.get('pipeline_naming', 'description_regex'))
 
 def validate_pipeline_name(pipeline_name):
     # Rule 1: Start with repo name - eg security, data, ai, ml, etc
     # no more than 5 letters
     # no less than 2 letters
     # lowercase
-    repo_name_regex = re.compile(r"^[a-z]{2,5}-")
     repo_name_match = repo_name_regex.match(pipeline_name)
     if not repo_name_match:
         return False
@@ -15,7 +20,6 @@ def validate_pipeline_name(pipeline_name):
     # no more than 6 words
     # no less than 2 words
     # Separate each word by a hyphen
-    description_regex = re.compile(r"(-[a-z]+){2,6}$")
     description_match = description_regex.search(pipeline_name)
     if not description_match:
         return False
@@ -23,12 +27,10 @@ def validate_pipeline_name(pipeline_name):
     return True
 
 if __name__ == "__main__":
-    example_pipeline_name = "ai-apply-sqlserver-cu"
-    if validate_pipeline_name(example_pipeline_name):
-        print(f"{example_pipeline_name} is a valid pipeline name")
-        os.environ["CI_COMMIT_PIPELINE"] = example_pipeline_name
+    pipeline_name = "ai-apply-sqlserver-cu"
+    if validate_pipeline_name(pipeline_name):
+        print(f"{pipeline_name} is a valid pipeline name")
     else:
-        print(f"{example_pipeline_name} is not a valid pipeline name")
-        exit(1)
+        print(f"{pipeline_name} is not a valid pipeline name")
 
 
