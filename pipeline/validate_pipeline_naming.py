3aba73c2cf939fe5d4d7ad05a6508820ab315847
EmmaMcC <76816350+Emma121288@users.noreply.github.com>
2023-04-27 15:07:16 +0100

    Update validate_pipeline_naming.py

--- pipeline/validate_pipeline_naming.py
+++ pipeline/validate_pipeline_naming.py
@@ -8,18 +8,10 @@ repo_name_regex = re.compile(config.get('pipeline_naming', 'repo_name_regex'))
 description_regex = re.compile(config.get('pipeline_naming', 'description_regex'))
 
 def validate_pipeline_name(pipeline_name):
-    # Rule 1: Start with repo name - eg security, data, ai, ml, etc
-    # no more than 5 letters
-    # no less than 2 letters
-    # lowercase
     repo_name_match = repo_name_regex.match(pipeline_name)
     if not repo_name_match:
         return False
-
-    # Rule 2: Follow repo name with clear description of what the pipeline does
-    # no more than 6 words
-    # no less than 2 words
-    # Separate each word by a hyphen
+    
     description_match = description_regex.search(pipeline_name)
     if not description_match:
         return False
