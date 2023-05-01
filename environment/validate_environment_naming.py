e60d3f8a01cff03773e067e313be846e17e25ea0
EmmaMcC <76816350+Emma121288@users.noreply.github.com>
2023-05-01 15:12:49 +0100

    Update validate_environment_naming.py

--- environment/validate_environment_naming.py
+++ environment/validate_environment_naming.py
@@ -27,8 +27,10 @@ if __name__ == "__main__":
 example_environment_name1 = "sec-001"
 example_environment_name2 = "dev-002"
 example_environment_name3 = "qa-001"
+example_environment_name4 = "908-new-environment"
 
 # print results
 print(validate_environment_name(example_environment_name1))
 print(validate_environment_name(example_environment_name2))
 print(validate_environment_name(example_environment_name3))
+print(validate_environment_name(example_environment_name4))
