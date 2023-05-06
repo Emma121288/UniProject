d50dfe8e3f9a9857b9259770bf80f1bd33794c9b
EmmaMcC <76816350+Emma121288@users.noreply.github.com>
2023-05-06 15:52:10 +0100

    Update validate_branch_naming.py

--- branch/validate_branch_naming.py
+++ branch/validate_branch_naming.py
@@ -5,28 +5,57 @@ from configparser import ConfigParser
 config = ConfigParser()
 config.read('config.ini')
 
+acronym_set = set(config.get('settings', 'acronym_set').split(','))
+category = set(config.get('settings', 'category').split(','))
 team_name_regex = re.compile(config.get('regex', 'team_name_regex'))
 work_category_regex = re.compile(config.get('regex', 'work_category_regex'))
 task_number_regex = re.compile(config.get('regex', 'task_number_regex'))
 task_description_regex = re.compile(config.get('regex', 'task_description_regex'))
 
 def validate_branch_name(branch_name):
-    team_name, work_category, task_number, task_description = branch_name.split("/")
+    if branch_name == 'main':
+        return True
+
+    branch_name_split = branch_name.split("/")
+    num_parts = len(branch_name_split)
+    if num_parts != 2 and num_parts != 3:
+        return False
+
+    team_name = branch_name_split[0]
+    work_category = branch_name_split[1]
+    task_number = ""
+    task_description = ""
+
+    if num_parts == 3:
+        task_parts = branch_name_split[2].split("-")
+        if len(task_parts) != 2:
+            return False
+        task_number, task_description = task_parts
 
     # validate team name
-    if not team_name_regex.match(team_name):
+    team_name_match = team_name_regex.match(team_name)
+    if not team_name_match:
         return False
 
     # validate work category
-    if not work_category_regex.match(work_category):
+    work_category_match = work_category_regex.match(work_category)
+    if not work_category_match:
         return False
 
     # validate task number
-    if not task_number_regex.match(task_number):
+    task_number_match = task_number_regex.match(task_number)
+    if not task_number_match:
         return False
 
     # validate task description
-    if not task_description_regex.match(task_description):
+    task_description_match = task_description_regex.match(task_description)
+    if not task_description_match:
+        return False
+
+    # validate branch name
+    if work_category in category:
+        pass
+    else:
         return False
 
     return True
